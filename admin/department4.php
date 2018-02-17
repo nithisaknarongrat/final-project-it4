1908ed2dc8066a0ca2dd7d716a59efff41f8b8bb
DREAM\nithi <nithi.saknarongrat28@gmail.com>
2018-02-18 02:14:55 +0700

    รวมหน้า department ทั้งหมดที่มีอยู่

---
+++ admin/department4.php
@@ -0,0 +1,50 @@
+<html><head>
+    <title>กองอายุรกรรมและจิตเวช</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="contact.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="logovgh.png">
+    <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+    <!--ปุ่ม scroll-->
+    <script src="jquery-1.4.2.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="scrolltopcontrol.js"></script>
+  </head><body>
+<!--ส่วน logo -->
+        <div class="section">
+            <div class="background-image" style="background-image : url('http://pingendo.github.io/pingendo-bootstrap/assets/blurry/800x600/7.jpg')"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-7 col-sm-5 col-md-5 col-lg-5">
+                        <img src="logo1.png" class="img-responsive">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--ส่วนเมนู-->
+			<div class="navbar navbar-default navbar-static-top" data-spy="affix" data-offset-top="190">
+				<?php include( "./menuweb.php");?>
+			</div>
+		<br>
+        <!--ส่วนหัวติดต่อ-->
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h4 class="text-center text-success">กองอายุรกรรมและจิตเวช</h4>
+                <hr>
+            </div>
+        </div>
+		<!--404page-->
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <img src="imageweb/404.png" class="center-block img-responsive img-rounded" width="500" height="200">
+                </div>
+            </div>
+            <br>
+		</div>
+        <hr>
+        <!--ส่วนท้าย-->
+        <?php include( "./footer.php");?>
+</body></html>
