5593a4cfbde422bfab0cd1daf3c1b898547b5d64
DREAM\nithi <nithi.saknarongrat28@gmail.com>
2018-02-18 01:57:20 +0700

    ส่วนท้ายของเว็บไซต์

---
+++ admin/footer.php
@@ -0,0 +1,23 @@
+<footer class="section section-success">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <h5 class="text-center" >โรงพยาบาลทหารผ่านศึก</h5>
+            <h6 class="text-center">123 ถนนวิภาวดีรังสิต แขวงสามเสนใน เขตพญาไท กรุงเทพมหานคร 10400
+              <br>โทร: 02-644-9400 โทรสาร 02-644-9416</h6>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="row">
+              <div class="col-xs-12 hidden-sm hidden-md hidden-lg text-right">
+                <a href="https://goo.gl/LVQLQx"><i class="fa fa-facebook fa-fw text-inverse"></i></a>
+              </div>
+            </div>
+            <div class="row">
+              <div class="hidden-xs col-sm-12 col-md-12 col-lg-12 text-right">
+                <a href="https://goo.gl/LVQLQx"><i class="fa fa-facebook fa-fw fa-lg text-inverse"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</footer>
