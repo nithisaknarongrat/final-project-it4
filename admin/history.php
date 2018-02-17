e220e02b214d2434eda664acc15dce71bd92ce27
DREAM\nithi <nithi.saknarongrat28@gmail.com>
2018-02-18 02:15:48 +0700

    หน้า history

---
+++ admin/history.php
@@ -0,0 +1,62 @@
+<html><head>
+        <title>ประวัติความเป็นมา</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="history.css" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="logovgh.png">
+        <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+        <!--ปุ่ม scroll-->
+        <script src="jquery-1.4.2.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="scrolltopcontrol.js"></script>
+    </head><body>
+        <!--ส่วน logo -->
+        <div class="section">
+            <div class="background-image" style="background-image : url('http://pingendo.github.io/pingendo-bootstrap/assets/blurry/800x600/7.jpg')"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-7 col-sm-5 col-md-5 col-lg-5">
+                        <img src="logo1.png" class="img-responsive">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--ส่วนเมนู-->
+			<div class="navbar navbar-default navbar-static-top" data-spy="affix" data-offset-top="190">
+				<?php include( "./menuweb.php");?>
+			</div>
+		<br>
+        <!--ส่วนหัวประวัติ-->
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h4 class="text-center text-success">ประวัติความเป็นมา</h4>
+                <hr>
+            </div>
+        </div>
+        <!--ส่วนหัวรูปวิสัยทัศน์-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="imageweb/history.jpg" class="center-block img-responsive img-rounded" width="500" height="200">
+                </div>
+            </div>
+            <br>
+            <!--ส่วนเนื้อหาวิสัยทัศน์-->
+            <div class="row">
+                <div class="col-md-12 text-justify">
+                    <h4 class="text-left text-success">ประวัติความเป็นมา :</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; โรงพยาบาลทหารผ่านศึก
+                    เป็นโรงพยาบาลที่ก่อตั้งโดยพระราชดำริของพระบาทสมเด็จพระเจ้าอยู่หัว แรกเริ่มได้ถือกำเนิดจากการสถานปฐมพยาบาลมีฐานะเป็นเพียงแผนกหนึ่งในกองสวัสดิการ
+                    องศ์การสงเคราะห์ทหารผ่านศึก ตามมติสภาฯ ครั้งที่ 2/91 เมื่อวันที่ 17 มิถุนายน
+                    พ.ศ. 2491 ให้ตั้งสถานพยาบาลขึ้น และในวันที่ 22 พฤศจิกายน พ.ศ. 2491 ได้จัดตั้งขึ้นเป็นหน่วยปฏิบัติการด้านการรักษาพยาบาลแก่ทหารผ่านศึกนอก
+                    ประจำการ เพื่อให้การสงเคราะห์ ตามระเบียบ อผศ.ฉบับที่ 3 ข้อ 5 ตั้งแต่ วันที่
+                    4 เมษายน พ.ศ. 2491 และ ให้บริการแก่ประชาชนในด้านการรักษาพยาบาลจ่ายค่ารักษาพยาบาลโดยคิดค่ารักษาตามสมควร</div>
+            </div>
+        </div>
+        <hr>
+        <!--ส่วนท้าย-->
+        <?php include( "./footer.php");?>
+    
+
+</body></html>
