d015f25f995c1b4c9edf56b7867cd2a93b1fbf47
DREAM\nithi <nithi.saknarongrat28@gmail.com>
2018-02-18 02:20:09 +0700

    หน้าวิสัยทัศน์

---
+++ admin/vision.php
@@ -0,0 +1,61 @@
+<html><head>
+        <title>วิสัยทัศน์ /พันธกิจ</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="contact.css" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="logovgh.png">
+        <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+        <!--ปุ่ม scroll-->
+        <script src="jquery-1.4.2.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="scrolltopcontrol.js"></script>
+    </head><body>
+        <!--ส่วน logo -->
+        <div class="section">
+            <div class="background-image" style="background-image : url('http://pingendo.github.io/pingendo-bootstrap/assets/blurry/800x600/7.jpg')"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-7 col-sm-5 col-md-5 col-lg-5">
+                        <img src="logo1.png" class="img-responsive">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--ส่วนเมนู-->
+			<div class="navbar navbar-default navbar-static-top" data-spy="affix" data-offset-top="190">
+				<?php include( "./menuweb.php");?>
+			</div>
+		<br>
+        <!--ส่วนหัววิสัยทัศน์-->
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h4 class="text-center text-success">วิสัยทัศน์ /&nbsp;พันธกิจ</h4>
+                <hr>
+            </div>
+        </div>
+        <!--ส่วนหัวรูปวิสัยทัศน์-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="imageweb/vision mistion.jpg" class="center-block img-responsive img-rounded" width="500" height="200">
+                </div>
+            </div>
+            <br>
+            <!--ส่วนเนื้อหาวิสัยทัศน์-->
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="text-left text-success">วิสัยทัศน์ :</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; โรงพยาบาลทหารผ่านศึกจะเป็นศูนย์กลางแห่งความเป็นเลิศในการดูแลรักษา
+                    ฟื้นฟูสมรรถภาพ ทั้งด้านร่างกายและจิตใจ ตลอดจนฝึกอาชีพแก่ทหารผ่านทหารผ่านศึก
+                    <h4 class="text-left text-success">พันธกิจ :</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ให้การรักษาพยาบาลทหารผ่านศึกครอบครัวทหารผ่านศึก
+                        และประชาชนทั่วไป ตามมาตรฐานคุณภาพและประสิทธิภาพ ธำรงไว้ซึ่งคุณธรรมและจริยธรรม
+                        เพื่อให้ผู้รับบริการมีความพึงพอใจสูงสุด</div>
+            </div>
+        </div>
+        <hr>
+        <!--ส่วนท้าย-->
+        <?php include( "./footer.php");?>
+    
+
+</body></html>
