39b84325d91136fc42638d8f4e9674d6ef5c6cdc
DREAM\nithi <nithi.saknarongrat28@gmail.com>
2018-02-18 02:12:41 +0700

    หน้าติดต่อ (เว็บไซต์)

---
+++ admin/contact.php
@@ -0,0 +1,68 @@
+<html><head>
+        <title>ติดต่อ</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="contact.css" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="logovgh.png">
+        <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+        <!--ปุ่ม scroll-->
+        <script src="jquery-1.4.2.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="scrolltopcontrol.js"></script>
+    </head><body>
+        <!--ส่วน logo -->
+        <div class="section">
+            <div class="background-image" style="background-image : url('http://pingendo.github.io/pingendo-bootstrap/assets/blurry/800x600/7.jpg')"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-7 col-sm-5 col-md-5 col-lg-5">
+                        <img src="logo1.png" class="img-responsive">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--ส่วนเมนู-->
+			<div class="navbar navbar-default navbar-static-top" data-spy="affix" data-offset-top="190">
+				<?php include( "./menuweb.php");?>
+			</div>
+		<br>
+        <!--ส่วนหัวติดต่อ-->
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h4 class="text-center text-success">ติดต่อเรา</h4>
+                <hr>
+            </div>
+        </div>
+        <!--ส่วนหัวรูปติดต่อ-->
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <img src="imageweb/รพ.(2).png" class="center-block img-responsive img-rounded" width="500" height="200">
+                </div>
+            </div>
+            <br>
+            <!--ส่วนเนื้อหาที่ติดต่อ-->
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <h4 class="text-left text-success">ที่อยู่โรงพยาบาลทหารผ่านศึก :</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    123 ถนนวิภาวดีรังสิต แขวงสามเสนใน เขตพญาไท กรุงเทพมหานคร 10400
+                    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; เบอร์โทรศัพท์ : 02-644-9400
+                    ,โทรสาร : 02-644-9416
+                    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;E-mail: vgh1970@hotmail.co.th
+                    <h4 class="text-left text-success">รูปแบบการเดินทาง :</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;รถยนต์ส่วนบุคคล
+                        <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;รถโดยสารสารธารณะ :
+                        <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;24, 69, 92, 107, 129, 138, ปอ.24,
+                        ปอ.69, ปอ.92, ปอ.129, ปอ.138
+                    <h4 class="text-left text-success">แผนที่เส้นทาง :</h4>&gt;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3258.5641897569685!2d100.55060375432524!3d13.772377047810306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x73538ff9391cc1d3!2z4LmC4Lij4LiH4Lie4Lii4Liy4Lia4Liy4LilIOC4l-C4q-C4suC4o-C4nOC5iOC4suC4meC4qOC4tuC4gQ!5e0!3m2!1sth!2sth!4v1495517759814" width="800" height="400" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <!--ส่วนท้าย-->
+		<?php include( "./footer.php");?>
+    
+
+</body></html>
